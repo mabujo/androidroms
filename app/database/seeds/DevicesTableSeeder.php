faff641f0567dc41a6900297d3e0068384c55166
John Henson <john@mabujo.com>
2013-06-11 13:29:11 +0200

    add devices seed data

--- app/database/seeds/DevicesTableSeeder.php
+++ app/database/seeds/DevicesTableSeeder.php
@@ -8,7 +8,9 @@ class DevicesTableSeeder extends Seeder {
     	// DB::table('devices')->delete();
 
         $devices = array(
-
+        		['name' => 'Galaxy S II I9100', 'manufacturer' => 'Samsung', 'type' => 'Phone', 'release_date' => '2011-05-02'],
+        		['name' => 'Galaxy Nexus', 'manufacturer' => 'Samsung', 'type' => 'Phone', 'release_date' => '2011-11-17'],
+        		['name' => 'One', 'manufacturer' => 'HTC', 'type' => 'Phone', 'release_date' => '2013-03-15']
         );
 
         // Uncomment the below to run the seeder
